841063bb041653e953f0fc041af4bfc4781c8d80
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-03-26 22:45:39 +0500

    Initialized Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/Auth/login/login.component';
+import { SignupComponent } from './Components/Auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
