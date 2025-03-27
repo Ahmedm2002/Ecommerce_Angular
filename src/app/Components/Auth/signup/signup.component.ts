4a4972c67e9a500711f2e7a950ce280abc8f9085
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-03-27 06:58:25 +0500

    added http client in app.conifg.ts providers

--- src/app/Components/Auth/signup/signup.component.ts
+++ src/app/Components/Auth/signup/signup.component.ts
@@ -7,6 +7,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { UsersService } from '../../../Services/UserServices/users.service';
+import { IUser } from '../../../Models/Interface/user.interface';
 @Component({
   selector: 'app-signup',
   imports: [FormsModule, ReactiveFormsModule],
@@ -14,33 +15,45 @@ import { UsersService } from '../../../Services/UserServices/users.service';
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  // constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) {}
 
   signUpForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email: new FormControl('', [
+    name: new FormControl('Ahmed', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    email: new FormControl('ahmed@a.com', [
       Validators.required,
       Validators.email,
       Validators.minLength(5),
     ]),
-    password: new FormControl('', [
+    password: new FormControl('asdfasf', [
       Validators.required,
       Validators.minLength(5),
     ]),
     role: new FormControl('user'),
   });
-  signup(evnet: Event) {
-    evnet.preventDefault();
-    console.log(this.signUpForm.value);
-    // this.userService.createUser(this.signUpForm.value).subscribe(
-    //   (res: any) => {
-    //     console.log(res);
-    //     alert(`user created: ${res}`);
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //     alert(`Error: ${error}`);
-    //   }
-    // );
+
+  signup(event: Event) {
+    event.preventDefault();
+    const { name, email, password } = this.signUpForm.value;
+    const userId = this.userService.generateUserId();
+    const newUser: IUser = {
+      name,
+      email,
+      password,
+      role: 'user',
+      userId,
+    };
+    this.userService.createUser(this.signUpForm.value).subscribe(
+      (res: any) => {
+        console.log(res);
+        alert(`user created: ${res}`);
+      },
+      (error) => {
+        console.log(error);
+        alert(`Error: ${error}`);
+      }
+    );
   }
 }
--- src/app/Services/UserServices/users.service.ts
+++ src/app/Services/UserServices/users.service.ts
@@ -1,16 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { IUser } from '../../Models/Interface/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor(private http: HttpClient) {}
+  http = inject(HttpClient);
+
+  charArray: string[] = [
+    ...'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+  ];
+  randomChar(): string {
+    return this.charArray[Math.floor(Math.random() * this.charArray.length)];
+  }
+  generateUserId() {
+    let userId = '';
+    const idLenght = 10;
+    for (let i = 0; i < idLenght; i++) {
+      userId += this.randomChar();
+    }
+    return userId;
+  }
 
   apiUrl = 'https://api.escuelajs.co/api/v1/';
 
   createUser(user: IUser) {
+    console.log(user);
     return this.http.post(`${this.apiUrl}users`, user);
   }
 }
